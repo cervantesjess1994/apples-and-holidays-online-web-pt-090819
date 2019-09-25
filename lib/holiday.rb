d1fe6cfd938dea8cf7952da6df51fd24d862bdf4
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-25 00:38:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,13 +59,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
-    holiday.each do |holiday, item|
-      puts "#{holiday.to_s.split("").map {|i| i.capitalize}.join("")}
-      :#{item.join(",")}"
+      puts "#{season.capitalize}:"
+      holiday.each do |holiday, supplies|
+      puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end
   end
-  holiday
 end
 
 def all_holidays_with_bbq(holiday_hash)
